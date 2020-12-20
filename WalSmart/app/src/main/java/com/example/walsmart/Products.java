775cefebd0a792fb82cc471058a132c3356b5ec5
224085 Barbara Borkowska <224085@edu.p.lodz.pl>
2020-12-20 16:04:52 +0100

    Product Activity edited.

--- WalSmart/app/src/main/java/com/example/walsmart/Products.java
+++ WalSmart/app/src/main/java/com/example/walsmart/Products.java
@@ -2,13 +2,16 @@ package com.example.walsmart;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SearchView;
+import android.widget.TextView;
 
 public class Products extends AppCompatActivity {
+    int quantity = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +30,21 @@ public class Products extends AppCompatActivity {
         SearchView searchView = (SearchView) findViewById(R.id.search_product);
         CharSequence query = searchView.getQuery();
 
+    }
+
+    public void increaseInteger(View view) {
+        if (quantity < 10) quantity++;
+        display(quantity);
+    }
+
+    public void decreaseInteger(View view) {
+        if (quantity > 0) quantity--;
+        display(quantity);
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void display(int number) {
+        TextView displayInteger = (TextView) findViewById(R.id.integer_number);
+        displayInteger.setText("" + number);
     }
 }
