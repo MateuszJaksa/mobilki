65b2f9e0218a19207cd7280adc808577c8f7f1cb
224085 Barbara Borkowska <224085@edu.p.lodz.pl>
2021-01-05 19:57:39 +0100

    list of products

--- WalSmart/app/src/main/java/com/example/walsmart/Product/ProductActivity.java
+++ WalSmart/app/src/main/java/com/example/walsmart/Product/ProductActivity.java
@@ -9,6 +9,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SearchView;
@@ -20,16 +21,23 @@ import com.example.walsmart.BasketActivity;
 import com.example.walsmart.Models.Product;
 import com.example.walsmart.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Objects;
 
 public class ProductActivity extends AppCompatActivity {
     int quantity = 0;
     public static RecyclerView products;
     private static ArrayList<Product> download_products = new ArrayList<>();
     private SearchView search_engine;
-    private ProductAdapter itemArrayAdapter = new ProductAdapter(R.layout.product_design, download_products);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,14 +72,35 @@ public class ProductActivity extends AppCompatActivity {
 
     private void getProductsFromDatabase() {
         download_products.clear();
-       /* for (Product t : *//*no z jakiejs listy produktow sciagnietych z bazy*//*)
-            download_products.add(t);*/
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+        Query query = mDatabase.child("products").orderByKey();
+        ValueEventListener queryValueListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Iterable<DataSnapshot> snapshotIterator = dataSnapshot.getChildren();
+                for (DataSnapshot next : snapshotIterator) {
+                    Product p = new Product(
+                            Objects.requireNonNull(next.child("name").getValue()).toString(),
+                            Objects.requireNonNull(next.child("photo").getValue()).toString(),
+                            Objects.requireNonNull(next.child("size").getValue()).toString(),
+                            Double.parseDouble(Objects.requireNonNull(next.child("price").getValue()).toString()));
+                    download_products.add(p);
+                    //Log.d("Debug", "Value = " + p.getName() + p.getPhoto() + p.getSize() + p.getPrice());
+                }
+            }
 
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
+        query.addListenerForSingleValueEvent(queryValueListener);
+        ProductAdapter itemsAdapter = new ProductAdapter(R.layout.product_design, download_products);
         products.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
         products.setItemAnimator(new DefaultItemAnimator());
         products.addItemDecoration(new DividerItemDecoration(getApplicationContext(), DividerItemDecoration.VERTICAL));
-        products.setAdapter(itemArrayAdapter);
-        ;
+        products.setAdapter(itemsAdapter);
+
     }
 
     public void increaseInteger(View view) {
